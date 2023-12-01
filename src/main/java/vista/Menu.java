69e5582357dd1381cc70a2520ac4ea8754aa1f3f
NichoUlloa <n.ulloa06@ufromail.cl>
2023-12-01 05:46:36 -0300

    se agregan clases curso, Estudiante

--- src/main/java/vista/Menu.java
+++ src/main/java/vista/Menu.java
@@ -1,43 +1,91 @@
 package vista;
+import model.Curso;
+import model.Estudiante;
 
 
 import java.util.Scanner;
 
 
 public class Menu {
+    public static void main(String[] args) {
+        Curso curso = new Curso();
+        menuPrincipal(curso);
+    }
 
-
-    public static void menuPrincipal(){
+    public static void menuPrincipal(Curso curso) {
         Scanner sc = new Scanner(System.in);
+
         int opcion = 0;
-        do{
-            System.out.println("Bienvenido al sistema de gestion de estudiantes");
-            System.out.println("1. Ingresar estudiante");
-            System.out.println("2. Listar estudiantes");
+        do {
+            System.out.println("Bienvenido al sistema de registro de estudiantes");
+            System.out.println("1. Registrar estudiante");
+            System.out.println("2. Mostrar estudiantes");
             System.out.println("3. Buscar estudiante");
             System.out.println("4. Eliminar estudiante");
             System.out.println("5. Salir");
-            System.out.println("Ingrese una opcion: ");
+            System.out.println("Ingrese una opción");
             opcion = sc.nextInt();
-            switch(opcion){
-                case 1: model.Curso.agregarEstudiante();
-                    System.out.println("Agregar estudiante");
+            switch (opcion) {
+                case 1:
+                    agregarEstudiante(curso);
                     break;
-                case 2: model.Curso.buscarEstudiante();
-                    System.out.println("Buscar estudiante");
-                case 3: model.Curso.mostrarEstudiantes();
-                    System.out.println("Mostrar estudiantes");
+                case 2:
+                    buscarEstudiante(curso);
                     break;
-                case 4: model.Curso.eliminarEstudiante();
-                    System.out.println("Eliminar estudiante");
+                case 3:
+                    eliminarEstudiante(curso);
+                    break;
+                case 4:
+                    mostrarEstudiantes(curso);
                     break;
                 case 5:
-                    System.out.println("Salir");
+                    System.out.println("Gracias por utilizar el sistema");
                     break;
                 default:
-                    System.out.println("Opcion no valida");
+                    System.out.println("Opción no válida");
                     break;
             }
-        }while(opcion != 5);
+        } while (opcion != 5);
+    }
+
+    //metodo agregar estudiante, con mensaje de exito o error
+    public static void agregarEstudiante(Curso curso) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese el nombre del estudiante");
+        String nombre = sc.next();
+        System.out.println("Ingrese el email del estudiante");
+        String email = sc.next();
+        System.out.println("Ingrese el rut del estudiante");
+        String rut = sc.next();
+        Estudiante estudiante = new Estudiante(nombre, email, rut);
+        curso.agregarEstudiante(estudiante);
+        System.out.println("Estudiante agregado con éxito");
+    }
+
+    //metodo buscar estudiante, con mensaje de exito o error
+    public static void buscarEstudiante(Curso curso) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese el rut del estudiante");
+        String rut = sc.next();
+        Estudiante estudiante = curso.buscarEstudiante(rut);
+        if (estudiante != null) {
+            System.out.println(estudiante.toString());
+        } else {
+            System.out.println("Estudiante no encontrado");
+        }
+    }
+
+    //metodo eliminar estudiante, con mensaje de exito o error
+    public static void eliminarEstudiante(Curso curso) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese el rut del estudiante");
+        String rut = sc.next();
+        curso.eliminarEstudiante(rut);
+        System.out.println("Estudiante eliminado con éxito");
+    }
+
+    //metodo mostrar lista estudiantes, con mensaje de exito o error
+    public static void mostrarEstudiantes(Curso curso) {
+        curso.mostrarEstudiantes();
     }
 }
